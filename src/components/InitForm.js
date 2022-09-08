af39645897564e30877427fefc2617ac44402a5d
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-09-07 23:00:13 -0400

    Trabalhando com eventos

--- src/components/InitForm.js
+++ src/components/InitForm.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 //Component
-const InitForm = (props) => {
+const InitForm = ({onClick, onChangeName}) => {
+  // const {onClick} = props;
   return (
     <form action='"'>
         <div className='form-group'>
@@ -13,10 +14,11 @@ const InitForm = (props) => {
               type="text"
               component="input"
               className='form-control'
+              onChange={onChangeName}
           />
         </div>
         <div className='form-group'>
-          <button className='btn btn-primary'>Send</button>
+          <button type='button' className='btn btn-primary' onClick={onClick}>Send</button>
         </div>
     </form>
   )
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,39 @@
+import React, {Component} from "react";
 import InitForm from "../components/InitForm";
 
 //Container
-function App() {
-  return (
-    <div className="col">
-        <p>
-          <div className="card">
-            <div className="card-header"> 
-              Card Header
-            </div>
-            <div className="card-body">
-               <div className="card-title">My First Redux Form</div>
-                <InitForm/>
-            </div>
-            <div className="card-footer">
-              <p>Card Footer</p>
-            </div>
+
+class App extends Component {
+  onClick=(e) =>{
+    e.preventDefault();
+    console.log('Clicked in Redux Form')
+  }
+
+  onChangeName = ({target}) =>{
+    const {name, value} = target;
+    console.log('Target input', name, value); 
+  }
+
+  render () {
+    return (
+      <div className="col">
+        <div className="card">
+          <div className="card-header"> 
+            Card Header
           </div>
-        </p>
-    </div>
-  );
+          <div className="card-body">
+            <div className="card-title">My First Redux Form</div>
+              <InitForm
+                onClick={this.onClick}
+                onChangeName={this.onChangeName}/>
+          </div>
+          <div className="card-footer">
+            <p>Card Footer</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
